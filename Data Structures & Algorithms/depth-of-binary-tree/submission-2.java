1ac1cabb8b0bd9764528ac5e96d13e0f729db01e
Victor Shcherbina <65561427+VicShcherbert@users.noreply.github.com>
2026-07-30 17:06:42 -0700

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root != null && root.left == null && root.right == null){
+            return 1;
+        }
+
+        int leftNumber = maxDepth(root.left);
+        int rightNumber = maxDepth(root.right);
+
+        return 1 + Math.max(leftNumber, rightNumber);
+    }
+}
